a127acde33dadab71efec447fa1d12585ce8625c
xfaraday <theonlyfaraday@gmail.com>
2022-10-17 00:15:40 -0700

    adding export and new art

--- scripter/scripts/linux/inventory.sh
+++ scripter/scripts/linux/inventory.sh
@@ -5,9 +5,105 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-RED='\033[0;31m'
-blue='\033[0;34m'
-NC='\033[0m'
+C=$(printf '\033')
+ORANGE="${C}[48;2;255;165;0m"
+RED="${C}[1;31m"
+WHITE="${C}[1;37m"
+BLACK="${C}[1;30m"
+SED_RED="${C}[1;31m&${C}[0m"
+GREEN="${C}[1;32m"
+SED_GREEN="${C}[1;32m&${C}[0m"
+YELLOW="${C}[1;33m"
+SED_YELLOW="${C}[1;33m&${C}[0m"
+SED_RED_YELLOW="${C}[1;31;103m&${C}[0m"
+BLUE="${C}[1;34m"
+SED_BLUE="${C}[1;34m&${C}[0m"
+ITALIC_BLUE="${C}[1;34m${C}[3m"
+LIGHT_MAGENTA="${C}[1;95m"
+SED_LIGHT_MAGENTA="${C}[1;95m&${C}[0m"
+LIGHT_CYAN="${C}[1;96m"
+SED_LIGHT_CYAN="${C}[1;96m&${C}[0m"
+LG="${C}[1;37m" #LightGray
+SED_LG="${C}[1;37m&${C}[0m"
+DG="${C}[1;90m" #DarkGray
+SED_DG="${C}[1;90m&${C}[0m"
+NC="${C}[0m"
+UNDERLINED="${C}[5m"
+ITALIC="${C}[3m"
+
+print_title(){
+  if [ "$DEBUG" ]; then
+    END_T2_TIME=$(date +%s 2>/dev/null)
+    if [ "$START_T2_TIME" ]; then
+      TOTAL_T2_TIME=$(($END_T2_TIME - $START_T2_TIME))
+      printf $DG"This check took $TOTAL_T2_TIME seconds\n"$NC
+    fi
+
+    END_T1_TIME=$(date +%s 2>/dev/null)
+    if [ "$START_T1_TIME" ]; then
+      TOTAL_T1_TIME=$(($END_T1_TIME - $START_T1_TIME))
+      printf $DG"The total section execution took $TOTAL_T1_TIME seconds\n"$NC
+      echo ""
+    fi
+
+    START_T1_TIME=$(date +%s 2>/dev/null)
+  fi
+
+  title=$1
+  title_len=$(echo $title | wc -c)
+  max_title_len=100
+  rest_len=$((($max_title_len - $title_len) / 2))
+
+  printf ${BLUE}
+  for i in $(seq 1 $rest_len); do printf " "; done
+  printf "╔"
+  for i in $(seq 1 $title_len); do printf "═"; done; printf "═";
+  printf "╗"
+
+  echo ""
+
+  for i in $(seq 1 $rest_len); do printf "═"; done
+  printf "╣ $GREEN${title}${BLUE} ╠"
+  for i in $(seq 1 $rest_len); do printf "═"; done
+
+  echo ""
+
+  printf ${BLUE}
+  for i in $(seq 1 $rest_len); do printf " "; done
+  printf "╚"
+  for i in $(seq 1 $title_len); do printf "═"; done; printf "═";
+  printf "╝"
+  
+  printf $NC
+  echo ""
+}
+
+print_2title(){
+  if [ "$DEBUG" ]; then
+    END_T2_TIME=$(date +%s 2>/dev/null)
+    if [ "$START_T2_TIME" ]; then
+      TOTAL_T2_TIME=$(($END_T2_TIME - $START_T2_TIME))
+      printf $DG"This check took $TOTAL_T2_TIME seconds\n"$NC
+      echo ""
+    fi
+
+    START_T2_TIME=$(date +%s 2>/dev/null)
+  fi
+
+  printf ${BLUE}"╔══════════╣ $GREEN$1\n"$NC #There are 10 "═"
+}
+
+print_3title(){
+  printf ${BLUE}"══╣ $GREEN$1\n"$NC #There are 2 "═"
+}
+
+print_list(){
+  printf ${BLUE}"═╣ $GREEN$1"$NC #There is 1 "═"
+}
+
+print_info(){
+  printf "${BLUE}╚ ${ITALIC_BLUE}$1\n"$NC
+}
 
 function usage() {
 	banner
@@ -25,36 +121,40 @@ function usage() {
 
 function banner() {
 	printf "
-				 /C.   C\.
-		   		/SS.   SS\.
-		  	   /UUU.   UUU\.
-		 	  /SSSS.   SSSS\.
-			 /BBBBB.   BBBBB\.
-	      /CSUSBCSUSBCSUSBCSUSB\.
-	  /CSUSBCSUSBCSUSBCSUSBCSUSBC\.
-	/CSUSBCSUSBCSUSBCSUSBCSUSBCSUSBCS\.
-  \CSUSBCSUS   USBCSUSBCSUSBC   BCSUSBCSU/.
-   \CSUSBCSUS   USBCSUSBCSUS   USBCSUSBC/.
-	\CSUSBCSUS   USBCSUSBCS   CSUSBCSUS/.
- 	 \CSUSBCSUS   USBCSUSB   SBCSUSBCS/.
-  	  \CSUSBCSUS   CSUSBC   SUSBCSUSB/.
-	   \CSUSBCSUSBCSUSBCSUSBCSUSBCSU/.
-		\CSUSBCSUSBCSUSBCSUSBCSUSBC/.
-	  		  \CSUSBCSUSBCSUS/.
-		   	   \CSUSBCSUSBCS/.
-				\CSUSBCSUSB/.
-				 \CSUSBCSU/.
-				  \CSUSBC/.
-				   \CSUS/.
-					\**/.\n\n" 
-
+                 ${DG}/C.   C\.
+                ${BLUE}/SS.   SS\.
+               /UUU.   UUU\.
+              /SSSS.   SSSS\.
+             ${DG}/BBBBB.   BBBBB\.
+          /CSUSBCSUSBCSUSBCSUSB\.
+        ${BLUE}/CSUSBCSUSBCSUSBCSUSBCSUSBC\.
+    /CSUSB${RED}CSUSB${BLUE}CSUSBCSUSBCSUS${RED}BCSUS${BLUE}BCS\.
+  \CSUSBCSU${RED}S${WHITE}&&&${RED}U${BLUE}SBCSUSBCSUSB${RED}C${WHITE}&&&${RED}B${BLUE}CSUSBCSU/.
+   \CSUSBCSU${RED}S${WHITE}&&&${RED}U${BLUE}SBCSUSBCSU${RED}S${WHITE}&&&${RED}U${BLUE}SBCSUSBC/.
+    \CSUSBCSU${RED}S${WHITE}&&&${RED}U${BLUE}SBCSUSBC${RED}S${WHITE}&&&${RED}C${BLUE}SUSBCSUS/.
+     \CSUSBCSU${RED}S${WHITE}&&&${RED}U${BLUE}SBCSUS${RED}B${WHITE}&&&${RED}S${BLUE}BCSUSBCS/.
+      \CSUSBCSU${RED}S${WHITE}&&&${RED}C${BLUE}SUSB${RED}C${WHITE}&&&${RED}S${BLUE}USBCSUSB/.
+       \CSUSBCSU${RED}SBCSU${BLUE}SB${RED}CSUSB${BLUE}CSUSBCSU/.
+        \CSUSBCSUSBCSUSBCSUSBCSUSBC/.
+              ${RED}\CSUSBCSUSBCSUS${RED}/${DG}.
+              ${WHITE}V${RED}\\${DG}CSUSBCSUSBCS${RED}/${WHITE}V${DG}.
+               ${WHITE}V${RED}\\${DG}CSUSBCSUSB${RED}/${WHITE}V${DG}.
+                ${WHITE}V${RED}\\${DG}CSUSBCSU${RED}/${WHITE}V${DG}.
+                 ${WHITE}V${RED}\\${DG}CSUSBC${RED}/${WHITE}V${DG}.
+                  ${WHITE}V${RED}\\${DG}CSUS${RED}/${WHITE}V${DG}.${WHITE}=\\${DG}      ~
+                   ${WHITE}V${RED}\**/${WHITE}V${DG}.${WHITE}\\==\\${DG}   ~
+                           ${WHITE}\\==\\${DG}    ~
+                            ${WHITE}\\==\\${DG} ~
+                             ${RED}(--)${DG}~~
+                             ~\n" 
+                           
 	printf "\t${RED}   ________________  ______\n${NC}"
 	printf "\t${RED}  / ____/ ____/ __ \/ ____/\n${NC}"
 	printf "\t${RED} / /   / /   / / / / /\n${NC}"     
 	printf "\t${RED}/ /___/ /___/ /_/ / /___\n${NC}"   
 	printf "\t${RED}\____/\____/_____/\____/\n${NC}" 
 
-	printf "\n${blue}BLUE TEAM INVENTORY\n\n${NC}"
+	printf "\n${BLUE}BLUE TEAM INVENTORY\n\n${NC}"
 }
 function spacer () {
 	printf "\n\n############################ $1 ############################\n\n"
@@ -63,20 +163,6 @@ function spacer () {
 function smallspacer () {
 	printf "\n############## $1 ##############\n"
 }
-
-function ExportToCSV () {
-	#figure out a way to make a newline for the csv file and fix it for the arrays
-	printf "\n\n${blue}Exporting to CSV...\n\n${NC}"
-	if ! [ -f ./inventory.csv ]; then
-		touch ./inventory.csv
-		runtime=$(date +"%H:%M:%S")
-		printf "$runtime," >> ./inventory.csv
-	fi
-	printf "$1," >> ./inventory.csv
-}
-
-#host and ip
-#hostname | ip 
 
 function host() {
 	host=$(hostname)
@@ -89,7 +175,7 @@ function host() {
 		ips+=($ip4)
 	done
 
-	section="${blue}OS INFORMATION${NC}"
+	section="${BLUE}OS INFORMATION${NC}"
 	spacer "$section"
 
 	#host info
@@ -103,37 +189,37 @@ function host() {
 }
 
 function user() {
-	section="${blue}USER INFORMATION${NC}"
+	section="${BLUE}USER INFORMATION${NC}"
 	spacer "$section"
 
 	#users
 	#cat /etc/passwd | grep -in /bin/bash
 	users=$(grep 'sh$' /etc/passwd)
-	section="${blue}Users that can login${NC}"
+	section="${BLUE}Users that can login${NC}"
 	smallspacer "$section"
 	printf "$users"
 
 	if [ -f /etc/sudoers ] ; then
-		section="${blue}Users in Sudoers File${NC}"
+		section="${BLUE}Users in Sudoers File${NC}"
 		smallspacer "$section"
 		awk '!/#(.*)|^$/' /etc/sudoers
 	fi 
 
 	if ! [ -z "grep sudo /etc/group" ]; then
-		section="${blue}Users in sudo group${NC}"
+		section="${BLUE}Users in sudo group${NC}"
 		smallspacer "$section"
 		grep -Po '^sudo.+:\K.*$' /etc/group
-		section="${blue}Users in admin group${NC}"
+		section="${BLUE}Users in admin group${NC}"
 		smallspacer "$section"
 		grep -Po '^admin.+:\K.*$' /etc/group
-		section="${blue}Users in wheel group${NC}"
+		section="${BLUE}Users in wheel group${NC}"
 		smallspacer "$section"
 		grep -Po '^wheel.+:\K.*$' /etc/group
 	fi
 }
 
 function login() {
-	section="${blue}Login Information${NC}"
+	section="${BLUE}Login Information${NC}"
 	smallspacer "$section"
 
 	printf "$(lastlog)\n\n"
@@ -142,22 +228,31 @@ function login() {
 }
 
 function ports() {
-	section="${blue}LISTENING CONNECTIONS${NC}"
-	spacer "$section"
+	if [  -z "$1" ]; then
+		section="${BLUE}LISTENING CONNECTIONS${NC}"
+		spacer "$section"
+		ports=$(lsof -i -P -n | grep LISTEN)
+		printf "$ports"
+	fi
 
 	function motherprocess() {
 		tmp=$1
-		until [ $tmp -eq 1 ]; do
+		tmpname=""
+		until [[ $tmp -eq 1 || $tmpname == "systemd" ]]; do
 			reg=$tmp
 			tmp=$(ps -o ppid= -p $reg)
+			tmpname=$(ps -fp $tmp | awk '{print $8}' | tail -n 1 | rev | cut -d '/' -f1 | rev | grep -Eo '[sS]ystemd')
 		done
 		cmd=$(ps -fp $reg | awk '{print $8}')
-		printf "Master process ID: $reg\n $cmd\n"
+		if [[ ! -z "$1" ]]; then
+			last=$(echo $cmd | tail -n 1 | awk {'print $2'})
+			printf "\"Service\":\"$last\"},"
+		else
+			printf "Master process ID: $reg\n $cmd\n"
+		fi
 	}
 
 	#ports
-	ports=$(lsof -i -P -n | grep LISTEN)
-	printf "$ports"
 	#find open ports lsof -i -P -n | grep LISTEN | awk '{print $9}' | cut -d':' -f2-
 	portlisten=$(lsof -i -P -n | grep LISTEN | awk '{print $9}' | cut -d':' -f2- | sort -u)
 	for i in $portlisten; do
@@ -168,9 +263,15 @@ function ports() {
 			#TO DO 
 			#also print the command used for last process, just in case
 			#
-			var=$(lsof -iTCP:$i -sTCP:LISTEN | awk '{print $2}' | tail -n1)
-			printf "\nPort: ${RED}$i${NC} Owning process: $var \n"
-			motherprocess "$var"
+			if [[ ! -z "$1" ]]; then
+				var=$(lsof -iTCP:$i -sTCP:LISTEN | awk '{print $2}' | tail -n1)
+				printf "{\"Port\":\"$i\","
+				motherprocess "$var"
+			else
+				var=$(lsof -iTCP:$i -sTCP:LISTEN | awk '{print $2}' | tail -n1)
+				printf "\nPort: ${RED}$i${NC} Owning process: $var \n"
+				motherprocess "$var"
+			fi	
 		fi
 	done
 }
@@ -185,8 +286,10 @@ function ports() {
 #systemctl is-active --quiet service && echo Service is running
 
 function service() {
-	section="${blue}SERVICES${NC}"
-	spacer "$section"
+	if [  -z "$1" ]; then
+		section="${BLUE}SERVICES${NC}"
+		spacer "$section"
+	fi
 	runningservs=()
 	essentials=("ssh" "sshd" "apache" "apache2" "httpd" "smbd" "vsftpd" "mysql" "postgresql" "vncserver" "xinetd" "telnetd" "webmin" "cups" "ntpd" "snmpd" "dhcpd" "ipop3" "postfix" "rsyslog" "docker" "samba" "postfix" "smtp" "psql" "clamav" "bind9" "nginx" "mariadb" "ftp")
 	for i in ${essentials[@]}; do
@@ -194,14 +297,22 @@ function service() {
 		if [ "$var" == "active" ]; then
 			secvar=$(systemctl is-enabled $i)
 			if [ "$secvar" == "enabled" ]; then
-				printf "Service: ${RED}$i${NC} is running and enabled!\n"
-				loc=$(ls /etc | grep $i)
-				printf "\t Likely config files: /etc/$loc\n"
+				if [[ ! -z "$1" ]]; then
+					printf "$i\n"
+				else
+					printf "Service: ${RED}$i${NC} is running and enabled!\n"
+					loc=$(ls /etc | grep $i)
+					printf "\t Likely config files: /etc/$loc\n"
+				fi
 			else
-				printf "Service: ${RED}$i${NC} is running!\n"
-				loc=$(ls /etc | grep $i)
-				printf "\t Likely config files: /etc/$loc\n"
-				runningservs+=("$i")
+				if [[ ! -z "$1" ]]; then
+					printf "$i\n"
+				else
+					printf "Service: ${RED}$i${NC} is running!\n"
+					loc=$(ls /etc | grep $i)
+					printf "\t Likely config files: /etc/$loc\n"
+					runningservs+=("$i")
+				fi
 			fi
 		fi
 	done
@@ -209,15 +320,15 @@ function service() {
 }
 
 function cron() { 
-	section="${blue}CRONTAB${NC}"
+	section="${BLUE}CRONTAB${NC}"
 	spacer "$section"
 
-	section="${blue}System Cronjobs${NC}"
+	section="${BLUE}System Cronjobs${NC}"
 	smallspacer "$section"
 
 	crontab -l
 
-	section="${blue}User Cronjobs${NC}"
+	section="${BLUE}User Cronjobs${NC}"
 	smallspacer "$section"
 
 	users=$(grep 'sh$' /etc/passwd | cut -d':' -f1)
@@ -228,7 +339,7 @@ function cron() {
 
 
 function log() {
-	section="${blue}LOGS${NC}"
+	section="${BLUE}LOGS${NC}"
 	spacer "$section"
 
 	#add log section
@@ -240,17 +351,17 @@ function dockerenum() {
 	runnin=$(docker ps)
 
 	images=$(docker images)
-	section="${blue}currently running${NC}"
+	section="${BLUE}currently running${NC}"
 	smallspacer "$section"
 	printf -- "$runnin\n\n"
-	section="${blue}images installed${NC}"
+	section="${BLUE}images installed${NC}"
 	smallspacer "$section"
 	printf -- "$images\n\n"
 
 }
 
 function dockercheck() {
-	section="${blue}DOCKER${NC}"
+	section="${BLUE}DOCKER${NC}"
 	spacer "$section"
 
 	if [ -x "$(command -v Docker)" ]; then
@@ -262,11 +373,8 @@ function dockercheck() {
 	fi
 }
 
-
-
-
 function file() {
-	section="${blue}FILES${NC}"
+	section="${BLUE}FILES${NC}"
 	spacer "$section"
 
 	#add file section
@@ -276,18 +384,41 @@ function file() {
 	printf "\n\n"
 }
 
-while getopts 'host:user:login:ports:services:cron:log:file:all' option; do
-	case "$option" in
-		v)host; exit 0 ;;
-		u)user; exit 0 ;;
-		l)login; exit 0 ;;
-		p)ports; exit 0 ;;
-		s)service; exit 0 ;;
-		c)cron; exit 0 ;;
-		o)dockercheck; exit 0 ;;
-		g)log; exit 0 ;;
-		f)file; exit 0 ;;
-		a)a= host; user; login; ports; service; cron; dockercheck; log; file; exit 0;;
-		h) usage; exit 0;;
-	esac
-done
+function ExportToJSON() {
+	webserv="httpbin.org/post"
+	printf "\n\n${BLUE}Exporting to JSON...\n\n${NC}"
+	JSON='{"name":"%s","ip":"%s","OS":"%s","services":[%s]}'
+	#create array with format of
+	#\{ "port": 80, "service": "http"},
+	#from the cracked lsof function
+	name=$(hostname)
+	IP=$(ip -brief a | head -n 3 | tail -n 1 | awk {'print $3'})
+	OS=$(hostnamectl | tail -n 3 | head -n 1 | awk {'print $3'})
+	services=$(ports "json")
+	#echo -e "${services::-1}\n\n"
+	postdata=$(printf "$JSON" "$name" "$IP" "$OS" "${services::-1}")
+	echo $postdata | jq
+	if [ -x $(which curl) ]; then
+		curl -d "$postdata" http://${webserv} #--insecure
+	else 
+		wget --post-data "$postdata" http://${webserv} #--no-check-certificate
+	fi
+}
+
+#while getopts 'banner:host:user:login:ports:services:cron:log:file:all' option; do
+#	case "$option" in
+#		b)banner; exit 0 ;;
+#		v)host; exit 0 ;;
+#		u)user; exit 0 ;;
+#		l)login; exit 0 ;;
+#		p)ports; exit 0 ;;
+#		s)service; exit 0 ;;
+#		c)cron; exit 0 ;;
+#		o)dockercheck; exit 0 ;;
+#		g)log; exit 0 ;;
+#		f)file; exit 0 ;;
+#		a)a= banner; host; user; login; ports; service; cron; dockercheck; log; file; exit 0;;
+#		h) usage; exit 0;;
+#	esac
+#done
+ExportToJSON
